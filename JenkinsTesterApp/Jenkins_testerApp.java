e46762dd9ee6364654dfae5ff492c03da49c34a6
Aditya Anasane <adityaanasane1@gmail.com>
2019-07-21 18:57:30 +0530

    added hello world program

---
+++ JenkinsTesterApp/Jenkins_testerApp.java
@@ -0,0 +1,12 @@
+package com.JenkinsTesterApp;
+
+public class Jenkins_testerApp {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<10;i++) {
+			System.out.println("Jenkins builded one!");
+		}
+	}
+
+}
